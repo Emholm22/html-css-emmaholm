9c7e8349f90285bc50bbcf0e7e7f777de711f4a6
Emma <emmhol71446@stud.noroff.no>
2025-09-13 20:18:39 +0200

    Create api.js

---
+++ api.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://v2.api.noroff.dev";
+
+export async function getProducts() {
+  const res = await fetch(`${BASE_URL}/rainy-days`);
+  if (!res.ok) throw new Error("Failed to load products.");
+  const json = await res.json();
+  return json.data; // array
+}
+
+export async function getProduct(id) {
+  const res = await fetch(`${BASE_URL}/rainy-days/${id}`);
+  if (!res.ok) throw new Error("Failed to load product.");
+  const json = await res.json();
+  return json.data; // single object
+}
